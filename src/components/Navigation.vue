4d2da0149aa66f0d8901cc481904f0865b21ed01
Lyudmil Mitev <lyudmil@mitev.me>
2021-04-10 17:56:07 +0200

    Refactor navigation component and fix event trigger bug

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,54 +2,25 @@
   <v-container>
     <v-list-item>
       <v-list-item-content>
-        <v-list-item-title class="title">
-          Memory Game
-        </v-list-item-title>
+        <v-list-item-title class="title"> Memory Game </v-list-item-title>
         <v-list-item-subtitle> Main Menu </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
-    <v-list
-      dense
-      nav
-    >
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon color="primary">
-            {{ play }}
-          </v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title @click="$emit('new-game')">
-            New Game
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon color="primary">
-            {{ info }}
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title @click="$emit('show-solution')">
-            Show Solution
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon color="primary">
-            {{ help }}
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title @click="$emit('show-help')">
-            How to play
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+    <v-list dense nav>
+      <v-list-item-group>
+        <v-list-item v-for="(item, index) in buttons" :key="index">
+          <v-list-item-icon>
+            <v-icon color="primary">
+              {{ item.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title @click="$emit(item.event)">
+              {{ item.label }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
     </v-list>
   </v-container>
 </template>
@@ -69,5 +40,11 @@ export default class Navigation extends Vue {
   info = mdiInformationOutline;
   help = mdiHelpCircleOutline;
   play = mdiPlayCircleOutline;
+
+  buttons = [
+    { label: "New game", event: "new-game", icon: this.play },
+    { label: "Show solution", event: "show-solution", icon: this.help },
+    { label: "How to play", event: "show-help", icon: this.info },
+  ];
 }
 </script>
